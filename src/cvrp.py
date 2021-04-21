9931bd32bba3c9ec735ff982526ea4893442c083
JKSenthil <senthil.jason@gmail.com>
2021-04-21 17:44:35 -0500

    init config work

--- src/cvrp.py
+++ src/cvrp.py
@@ -34,32 +34,52 @@ class CVRP:
                 return False
         return True
 
-    def simulated_annealing(self, temperature=1, cooling_rate=0.95, max_iter=1000):
-        def neighbor(r):
+    def simulated_annealing(self, temperature=1, cooling_rate=0.95, max_iter=10000):
+        def RHA(r):
             r_prime = [[i for i in row] for row in r] # same as deepcopy(r)
-            r1_idx = int(random() * len(r))
-            r2_idx = int(random() * len(r))
-            while len(r[r1_idx]) == 0 or len(r[r2_idx]) == 0:
-                r1_idx = int(random() * len(r))
-                r2_idx = int(random() * len(r))
-            
-            c1_idx = int(random() * len(r[r1_idx]))
-            c2_idx = int(random() * len(r[r2_idx]))
-            r_prime[r1_idx][c1_idx], r_prime[r2_idx][c2_idx] = r_prime[r2_idx][c2_idx], r_prime[r1_idx][c1_idx]
-            count = 0
-            while not (self._cap_constraint(r_prime[r1_idx]) and self._cap_constraint(r_prime[r2_idx])):
-                r_prime = [[i for i in row] for row in r]
-                c1_idx = int(random() * len(r[r1_idx]))
-                c2_idx = int(random() * len(r[r2_idx]))
+            highest_avg_customer_idx = -1
+            route_idx = -1
+            max_cost = float("-inf")
+            for j, route in enumerate(r):
+                if len(route) == 0 or len(route) == 1:
+                    continue
+                c = self.dist[0][route[0]] + self.dist[route[0]][route[1]]
+                if c > max_cost:
+                    highest_avg_customer_idx = 0
+                    route_idx = j
+                    max_cost = c
+                for i in range(1, len(route)-1):
+                    c = self.dist[route[i-1]][route[i]] + self.dist[route[i]][route[i+1]]
+                    if c > max_cost:
+                        highest_avg_customer_idx = i
+                        route_idx = j
+                        max_cost = c
+                c = self.dist[route[-2]][route[-1]] + self.dist[route[-1]][0]
+                if c > max_cost:
+                    highest_avg_customer_idx = len(route) - 1
+                    route_idx = j
+                    max_cost = c
 
-                r_prime[r1_idx][c1_idx], r_prime[r2_idx][c2_idx] = r_prime[r2_idx][c2_idx], r_prime[r1_idx][c1_idx]
-                count += 1
-                if count == 50:
-                    return r
+            assert highest_avg_customer_idx != -1
+            assert route_idx != -1
+
+            customer_idx = r_prime[route_idx].pop(highest_avg_customer_idx)
+
+            route_idxs = list(range(len(r)))
+            shuffle(route_idxs)
+            inserted = False
+            for r_idx in route_idxs:
+                r_prime[r_idx].append(customer_idx)
+                if not self._cap_constraint(r_prime[r_idx]):
+                    r_prime[r_idx].pop()
+                else:
+                    inserted = True
+                    break
+            if not inserted:
+                print("not inserted")
             return r_prime
 
-
-        routes = self._generate_initial_config()
+        routes = self._generate_initial_configV2()
         for i in range(len(routes)):
             if len(routes[i]) <= 1:
                 continue
@@ -70,7 +90,9 @@ class CVRP:
 
         for j in range(max_iter):
             print(j)
-            new_routes = neighbor(routes)
+            new_routes = RHA(routes)
+            for _ in range(4):
+                new_routes = RHA(new_routes)
             for i in range(len(new_routes)):
                 if len(new_routes[i]) <= 1:
                     continue
@@ -111,7 +133,71 @@ class CVRP:
 
         return routes
 
-    def _tsp_simulated_annealing(self, route, temperature=1, cooling_rate=0.95, max_iter=1000):
+    def _generate_initial_configV2(self):
+        def shuffle_tiny_bit(a, times=1):
+            for _ in range(times):
+                i = int(random() * len(a))
+                j = int(random() * len(a))
+                a[i], a[j] = a[j], a[i]
+            return a
+
+        routes = [[] for _ in range(self.num_vehicles)]
+        rolling_capacities = [0 for _ in range(self.num_customers)]
+        customer_idxs = list(range(1,self.num_customers))
+        customer_idxs.sort(key=lambda x : self.dist[x][0])
+
+        while customer_idxs:
+            idx = customer_idxs.pop(0)
+            customer_allocated = False
+            for i in range(self.num_vehicles):
+                if rolling_capacities[i] + self.customer_demand[idx] <= self.vehicle_capacity:
+                    rolling_capacities[i] += self.customer_demand[idx]
+                    routes[i].append(idx)
+                    customer_allocated = True
+                    break
+            
+            if not customer_allocated:
+                routes = [[] for _ in range(self.num_vehicles)]
+                rolling_capacities = [0 for _ in range(self.num_customers)]
+                customer_idxs = list(range(1,self.num_customers))
+                customer_idxs.sort(key=lambda x : self.dist[x][0])
+                customer_idxs = shuffle_tiny_bit(customer_idxs, times=3)
+
+        return routes
+
+    def _generate_initial_configV3(self):
+        routes = [[] for _ in range(self.num_vehicles)]
+        rolling_capacities = [0 for _ in range(self.num_customers)]
+        customer_idxs = list(range(1,self.num_customers))
+        customer_idxs.sort(key=lambda x : self.dist[x][0])
+
+        for i in range(self.num_vehicles):
+            if len(customer_idxs) == 1:
+                routes[i].append(customer_idxs.pop())
+                break
+
+            idx1 = customer_idxs.pop(0)
+            idx2 = customer_idxs.pop(0)
+            rolling_capacities[i] = self.customer_demand[idx1] + self.customer_demand[idx2]
+            routes[i].append(idx1)
+            while True:
+                if len(customer_idxs) == 0:
+                    break
+                idx = customer_idxs.pop()
+                if rolling_capacities[i] + self.customer_demand[idx] > self.vehicle_capacity:
+                    customer_idxs.append(idx)
+                    break
+                else:
+                    rolling_capacities[i] += self.customer_demand[idx]
+                    routes[i].append(idx)
+            routes[i].append(idx2)
+        
+        if len(customer_idxs) != 0:
+            assert 1 == 0 # force error to raise
+
+        return routes
+
+    def _tsp_simulated_annealing(self, route, temperature=1, cooling_rate=0.95, max_iter=10):
         """
         https://www.baeldung.com/java-simulated-annealing-for-traveling-salesman
         """
