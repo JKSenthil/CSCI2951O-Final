109ce1e586b073d599f410e923007ed082fbe027
Kalvin Lam <ikalvinz@gmail.com>
2021-04-22 11:39:15 -0700

    removing old code

--- src/visualize.py
+++ src/visualize.py
@@ -25,25 +25,3 @@ if __name__ == "__main__":
     
     writeFile(filename + "_initial", initial_config, cvrp.compute_obj_value(initial_config))
     writeFile(filename, best_config, cvrp.compute_obj_value(best_config))
-
-
-    # new_config = []
-    # for route in initial_config:
-    #     if len(route) > 2:
-    #         new_route, _ = cvrp._tsp_simulated_annealing(route)
-    #         new_config.append(new_route)
-    #     else:
-    #         new_config.append(route)
-    # cost = cvrp._compute_obj_value(new_config)
-
-    # s = ""
-    # for route in new_config:
-    #     s += "0 "
-    #     for e in route:
-    #         s += str(e) + " "
-    #     s += "0\n"
-
-    # f = open(filename + "2.sol", "w")
-    # f.write(f"{cost} 0\n")
-    # f.write(s)
-    # f.close
